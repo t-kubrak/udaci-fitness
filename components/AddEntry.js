af7739f9475bca4c93c8226cadb8c72bfc9b11f0
t-kubrak <taras.m.kubrak@gmail.com>
2021-03-03 17:36:27 +0200

    feat: add storage api

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from "./UdaciSteppers";
 import DateHeader from "./DateHeader";
 import { Ionicons } from "@expo/vector-icons";
 import TextButton from "./TextButton";
+import { submitEntry, removeEntry } from '../utils/api'
 
 function SubmitBtn({ onPress }) {
     return (
@@ -60,7 +61,7 @@ export default class AddEntry extends Component {
 
         // Navigate to home
 
-        // Save to "DB"
+        submitEntry({ key, entry })
 
         // Clear local notification
     };
@@ -70,8 +71,7 @@ export default class AddEntry extends Component {
         // Update Redux
 
         // Route to Home
-
-        // Update "DB"
+        removeEntry(key)
     };
     render() {
         const metaInfo = getMetricMetaInfo();
