859c8fd77b038314e12dae4d8b8c48450801e2c4
t-kubrak <taras.m.kubrak@gmail.com>
2021-03-04 18:46:57 +0200

    feat: add tab navigator v5

--- App.js
+++ App.js
@@ -6,14 +6,54 @@ import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { purple, white } from './utils/colors'
+
+const Tab =
+    Platform.OS !== 'ios'
+        ? createBottomTabNavigator()
+        : createMaterialTopTabNavigator()
+
 export default function App() {
-  return (
-      <Provider store={createStore(reducer)}>
-        <View style={{flex: 1}}>
-            <History />
-        </View>
-      </Provider>
-  );
+    return (
+        <Provider store={createStore(reducer)}>
+            <NavigationContainer>
+                <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: ({ color, size }) => {
+                            let icon
+                            if (route.name === 'Add Entry') {
+                                icon = (
+                                    <FontAwesome name="plus-square" size={size} color={color} />
+                                )
+                            } else if (route.name === 'History') {
+                                icon = (
+                                    <Ionicons name="ios-bookmarks" size={size} color={color} />
+                                )
+                            }
+                            return icon
+                        },
+                    })}
+                    tabBarOptions={{
+                        activeTintColor: Platform.OS === 'ios' ? purple : white,
+                        style: {
+                            backgroundColor: Platform.OS === 'ios' ? white : purple,
+                        },
+                        indicatorStyle: {
+                            // Android tab indicator (line at the bottom of the tab)
+                            backgroundColor: 'yellow',
+                        },
+                    }}
+                >
+                    <Tab.Screen name="History" component={History} />
+                    <Tab.Screen name="Add Entry" component={AddEntry} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </Provider>
+    )
 }
 
 const styles = StyleSheet.create({
