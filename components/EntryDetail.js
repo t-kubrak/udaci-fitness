b9eb6dee0bd37ea8fac5a36d632f778993aa8bff
t-kubrak <taras.m.kubrak@gmail.com>
2021-03-04 19:52:52 +0200

    feat: add date to navigation

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -2,10 +2,27 @@ import React, {Component} from 'react'
 import {Text, View} from 'react-native'
 
 class EntryDetail extends Component {
+
+    componentDidMount(){
+        this.setTitle(this.props.route.params.entryId);
+    }
+
+    setTitle = (entryId) => {
+        if (!entryId) return;
+        const year = entryId.slice(0, 4)
+        const month = entryId.slice(5, 7)
+        const day = entryId.slice(8)
+
+        this.props.navigation.setOptions({
+            title: `${month}/${day}/${year}`
+        });
+    };
+
     render() {
+        const {entryId} = this.props.route.params;
         return (
             <View>
-                <Text>Entry Detail - {JSON.stringify(this.props.route.params.entryId)}</Text>
+                <Text> Entry Detail - {JSON.stringify(entryId)} </Text>
             </View>
         )
     }
