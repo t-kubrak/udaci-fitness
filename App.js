e19d69b274e57877ba90f041507abdb8d6a2c178
t-kubrak <taras.m.kubrak@gmail.com>
2021-03-04 18:53:42 +0200

    feat: update status bar

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar, View } from 'react-native';
 import AddEntry from "./components/AddEntry";
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
-
+import Constants from 'expo-constants';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { purple, white } from './utils/colors'
 
+function UdaciStatusBar ({backgroundColor, ...props}) {
+    return (
+        <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+            <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+        </View>
+    )
+}
+
 const Tab =
-    Platform.OS !== 'ios'
+    Platform.OS === 'ios'
         ? createBottomTabNavigator()
         : createMaterialTopTabNavigator()
 
 export default function App() {
     return (
         <Provider store={createStore(reducer)}>
+            <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
             <NavigationContainer>
                 <Tab.Navigator
                     screenOptions={({ route }) => ({
