1cd45f0c442ebe269da796595acba1dc1e9d4364
t-kubrak <taras.m.kubrak@gmail.com>
2021-03-05 11:00:55 +0200

    feat: add live tab

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
-import {View, StatusBar } from 'react-native'
-import { Constants } from 'react-native-unimodules'
-
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-import reducer from './reducers'
-
-import AddEntry from './components/AddEntry'
-import History from './components/History'
-import EntryDetail from './components/EntryDetail'
-import { gray, orange, purple, white } from './utils/colors'
-
-import {NavigationContainer} from '@react-navigation/native'
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { View, StyleSheet, Platform, StatusBar } from 'react-native';
+import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import History from './components/History'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { white, purple } from './utils/colors'
+import Constants from 'expo-constants'
+import EntryDetail from './components/EntryDetail'
 import { createStackNavigator } from '@react-navigation/stack';
+import Live from './components/Live'
 
-
-function UdaciStatusBar({backgroundColor, ...props}){
+function UdaciStatusBar ({backgroundColor, ...props}){
     return (
         <View style={{backgroundColor, height: Constants.statusBarHeight}}>
             <StatusBar translucent backgroundColor={backgroundColor} {...props} />
@@ -24,46 +24,99 @@ function UdaciStatusBar({backgroundColor, ...props}){
     )
 }
 
-const Tab = createMaterialTopTabNavigator();
-function TabNaviagtor(){
-    return (
-        <Tab.Navigator
-            tabBarOptions={
-                {
-                    activeTintColor: white,
-                    inactiveTintColor: gray,
-                    style: {backgroundColor: purple},
-                    tabStyle: {height: 54},
-                    indicatorStyle: {backgroundColor: orange}
-                }
-            }>
-            <Tab.Screen name="History" component={History}/>
-            <Tab.Screen name="Add Entry" component={AddEntry}/>
-        </Tab.Navigator>
-    )
+// Config for TabNav
+const RouteConfigs = {
+    History:{
+        name: "History",
+        component: History,
+        options: {tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />, title: 'History'}
+    },
+    AddEntry:{
+        component: AddEntry,
+        name: "Add Entry",
+        options: {tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor} />, title: 'Add Entry'}
+    },
+    Live:{
+        component: Live,
+        name: "Live",
+        options: {tabBarIcon: ({tintColor}) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />, title: 'Live'}
+    }
 }
 
+const TabNavigatorConfig = {
+    navigationOptions: {
+        header: null
+    },
+    tabBarOptions: {
+        activeTintColor: Platform.OS === "ios" ? purple : white,
+        style: {
+            height: 56,
+            backgroundColor: Platform.OS === "ios" ? white : purple,
+            shadowColor: "rgba(0, 0, 0, 0.24)",
+            shadowOffset: {
+                width: 0,
+                height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+        }
+    }
+};
+
+const Tab = Platform.OS === 'ios'
+    ? createBottomTabNavigator()
+    : createMaterialTopTabNavigator()
+
+const TabNav = () =>(
+    <Tab.Navigator {...TabNavigatorConfig}>
+        <Tab.Screen {...RouteConfigs['History']} />
+        <Tab.Screen {...RouteConfigs['AddEntry']} />
+        <Tab.Screen {...RouteConfigs['Live']} />
+    </Tab.Navigator>
+)
+
+// Config for StackNav
+const StackNavigatorConfig = {
+    headerMode: "screen"
+}
+const StackConfig = {
+    TabNav:{
+        name: "Home",
+        component: TabNav,
+        options: {headerShown: false}
+    },
+    EntryDetail:{
+        name: "EntryDetail",
+        component: EntryDetail,
+        options: {
+            headerTintColor: white,
+            headerStyle:{
+                backgroundColor: purple
+            }
+        }
+    }
+}
 const Stack = createStackNavigator();
-function StackNavigator() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={TabNaviagtor} options={{headerShown: false}}/>
-            <Stack.Screen name="EntryDetail" component={EntryDetail} options={{headerTintColor: white, headerStyle: {backgroundColor: purple}}}  />
-        </Stack.Navigator>
-    );
-}
+const MainNav = () =>(
+    <Stack.Navigator {...StackNavigatorConfig}>
+        <Stack.Screen {...StackConfig['TabNav']} />
+        <Stack.Screen {...StackConfig['EntryDetail']} />
+    </Stack.Navigator>
+)
 
-export default class App extends Component {
-    render() {
-        return (
-            <Provider store={createStore(reducer)}>
-                <View style={{flex: 1}}>
+// App
+export default class App extends React.Component{
+    render(){
+        const store = createStore(reducer)
+        return(
+            <Provider store={store}>
+                <View style={{flex:1}}>
                     <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
-                    <NavigationContainer>
-                        <StackNavigator />
+                    <NavigationContainer >
+                        <MainNav />
                     </NavigationContainer>
                 </View>
             </Provider>
-        );
+        )
     }
 }
