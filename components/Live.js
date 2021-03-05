eebbee46daa06c9ac205f693a914b84a9d91ac59
t-kubrak <taras.m.kubrak@gmail.com>
2021-03-05 14:02:17 +0200

    feat: live, ask permission

--- components/Live.js
+++ components/Live.js
@@ -28,7 +28,15 @@ export default class Live extends Component {
             })
     }
     askPermission = () => {
+        Permissions.askAsync(Permissions.LOCATION)
+            .then(({ status }) => {
+                if (status === 'granted') {
+                    return this.setLocation()
+                }
 
+                this.setState(() => ({ status }))
+            })
+            .catch((error) => console.warn('error asking Location permission: ', error))
     }
     setLocation = () => {
         Location.watchPositionAsync({
@@ -85,7 +93,7 @@ export default class Live extends Component {
                 <View style={styles.directionContainer}>
                     <Text style={styles.header}>You're heading</Text>
                     <Text style={styles.direction}>
-                        North
+                        {direction}
                     </Text>
                 </View>
                 <View style={styles.metricContainer}>
@@ -94,7 +102,7 @@ export default class Live extends Component {
                             Altitude
                         </Text>
                         <Text style={[styles.subHeader, {color: white}]}>
-                            {200} feet
+                            {Math.round(coords.altitude * 3.2808)} Feet
                         </Text>
                     </View>
                     <View style={styles.metric}>
@@ -102,7 +110,7 @@ export default class Live extends Component {
                             Speed
                         </Text>
                         <Text style={[styles.subHeader, {color: white}]}>
-                            {300} MPH
+                            {(coords.speed * 2.2369).toFixed(1)} MPH
                         </Text>
                     </View>
                 </View>
