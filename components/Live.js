78f5f05bdf397716bb2792104c7fbbbb71d26668
t-kubrak <taras.m.kubrak@gmail.com>
2021-03-05 14:45:25 +0200

    feat: live, add animation

--- components/Live.js
+++ components/Live.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { purple, white } from '../utils/colors'
 import * as Location from 'expo-location';
@@ -10,7 +10,8 @@ export default class Live extends Component {
     state = {
         coords: null,
         status: null,
-        direction: ''
+        direction: '',
+        bounceValue: new Animated.Value(1),
     }
     componentDidMount () {
         Permissions.getAsync(Permissions.LOCATION)
@@ -47,6 +48,13 @@ export default class Live extends Component {
             const newDirection = calculateDirection(coords.heading)
             const { direction, bounceValue } = this.state
 
+            if (newDirection !== direction) {
+                Animated.sequence([
+                    Animated.timing(bounceValue, { duration: 200, toValue: 1.04}),
+                    Animated.spring(bounceValue, { toValue: 1, friction: 4})
+                ]).start()
+            }
+
             this.setState(() => ({
                 coords,
                 status: 'granted',
@@ -55,7 +63,7 @@ export default class Live extends Component {
         })
     }
     render() {
-        const { status, coords, direction } = this.state
+        const { status, coords, direction, bounceValue } = this.state
 
         if (status === null) {
             return <ActivityIndicator style={{marginTop: 30}}/>
@@ -76,9 +84,10 @@ export default class Live extends Component {
             return (
                 <View style={styles.center}>
                     <Foundation name='alert' size={50} />
-                    <Text>
-                        You need to enable location services for this app.
-                    </Text>
+                    <Animated.Text
+                        style={[styles.direction, {transform: [{scale: bounceValue}]}]}>
+                        {direction}
+                    </Animated.Text>
                     <TouchableOpacity style={styles.button} onPress={this.askPermission}>
                         <Text style={styles.buttonText}>
                             Enable
